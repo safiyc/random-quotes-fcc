08ba9566d332ac10dcc7436da13bcab4fece4267
Safiy Cham <csafiy@yahoo.com>
2019-10-07 10:31:05 -0700

    Make simple api request for raw format of my gist quotes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      quotes: []
+    };
+  }
+
+  componentDidMount() {
+    const bypassCors = "https://cors-anywhere.herokuapp.com/";
+    // const bypassCors = "https://crossorigin.me/";
+
+    // raw format of my gist 'quotes.json'
+    // when gist updated, must provide new link of raw
+    const quotesJSON = "https://gist.githubusercontent.com/safiyc/00897047c3fed1757971379046308134/raw/8fc6ed48323c4a836605a9df3aa63d1f8d63efc7/quotes.json";
+
+    fetch(bypassCors + quotesJSON)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            quotes: result.quotes
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, quotes } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (isLoaded === false) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div id='quote-box'>
+          <ul>
+            {quotes.map(el => (
+              <li key={el.quote} id='text'>
+                {el.quote} <span id='author'>{el.author}</span>
+              </li>
+            ))}
+          </ul>
+          <button id='new-quote'>New Quote</button>
+          <button id='tweet-quote'>Tweet Quote</button>
+        </div>
+      )
+    }
+  }
 }
 
 export default App;
